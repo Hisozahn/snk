bc18c6d756331833c30f016877082315a56d525c
Igor <romigor96@gmail.com>
2019-10-19 11:12:05 +0300

    Remove unused assert function

--- snk_util.h
+++ snk_util.h
@@ -10,12 +10,6 @@ extern "C" {
 #define SNK_ARRAY_LEN(_array)    (sizeof(_array) / sizeof((_array)[0]))
 #define SNK_RAND_MAX    32767
 
-inline void snk_assert(int expr)
-{
-    if (!expr)
-        fprintf(stderr, "assert failed\n");
-}
-
 static inline uint32_t snk_rand(uint32_t *seed)
 {
     *seed = ((*seed * 1103515245 + 12345) / 65536) % SNK_RAND_MAX;
