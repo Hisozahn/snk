9eae5afd4614f04bf241d04f53c3197fd18a327e
Igor <romigor96@gmail.com>
2019-10-22 19:37:34 +0300

    Document defs and util

--- snk_defs.h
+++ snk_defs.h
@@ -8,23 +8,33 @@
 extern "C" {
 #endif
 
+/**
+ * Status code.
+ */
 typedef enum snk_rc_type {
-    SNK_RC_SUCCESS,
-    SNK_RC_OVER,
-    SNK_RC_INVALID,
-    SNK_RC_NOBUF,
-    SNK_RC_NOENT,
+    SNK_RC_SUCCESS, /**< OK */
+    SNK_RC_OVER, /**< Game over condition is reached */
+    SNK_RC_INVALID, /**< Invalid arguments/object state */
+    SNK_RC_NOBUF, /**< Buffer size is too small */
+    SNK_RC_NOENT, /**< No such entry */
 } snk_rc_type;
 
 /** Direction of an object */
 typedef enum snk_direction {
-    SNK_DIRECTION_LEFT,
-    SNK_DIRECTION_UP,
-    SNK_DIRECTION_RIGHT,
-    SNK_DIRECTION_DOWN,
-    SNK_DIRECTION_INVALID,
+    SNK_DIRECTION_LEFT, /**< Towards negative x coordinate */
+    SNK_DIRECTION_UP, /**< Towards negative y coordinate */
+    SNK_DIRECTION_RIGHT, /**< Towards positive x coordinate */
+    SNK_DIRECTION_DOWN, /**< Towards positive y coordinate */
+    SNK_DIRECTION_INVALID, /**< Invalid direction */
 } snk_direction;
 
+/**
+ * Get opposite direction.
+ *
+ * @param[in] direction     Original direction
+ *
+ * @return                  Opposite direction
+ */
 static inline snk_direction
 snk_direction_reverse(snk_direction direction)
 {
--- snk_util.h
+++ snk_util.h
@@ -5,9 +5,17 @@
 extern "C" {
 #endif
 
+/** Get number of elements in an array */
 #define SNK_ARRAY_LEN(_array)    (sizeof(_array) / sizeof((_array)[0]))
+/** Maximum number that can be produced by snk_rand */
 #define SNK_RAND_MAX    32767
 
+/**
+ * Produce pseudo-random number
+ *
+ * @param[in,out] seed  Random seed
+ * @return              Random number
+ */
 static inline uint32_t snk_rand(uint32_t *seed)
 {
     *seed = ((*seed * 1103515245 + 12345) / 65536) % SNK_RAND_MAX;
